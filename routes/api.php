a9ae139c397ba280f59c103358fcce8e6df8c039
Qismat Huseyn <50789493+Qismet60@users.noreply.github.com>
2020-09-19 02:11:39 +0300

    Confiquration router web/api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\SpecializationController;
+use App\Http\Controllers\UniversityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::prefix('specialization')->group(function () {
+        Route::get('/', [SpecializationController::class, 'index'])->name('get.specialization');
+        Route::post('store', [SpecializationController::class, 'store'])->name('store.specialization')->middleware('auth');
+    });
+
+    Route::prefix('universities')->group(function () {
+        Route::post('store', [UniversityController::class, 'store'])->name('store.university')->middleware('auth');
+    });
 });
